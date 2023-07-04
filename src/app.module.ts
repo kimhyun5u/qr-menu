d5ecfd0f106617bd1e81458bbfa0ebd58b7731a2
kimhyun5u <gustndlfrl@gmail.com>
2023-07-04 11:41:35 +0900

    feat: add serve-static module 정적 파일 접근 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RestaurantsModule } from './restaurants/restaurants.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), RestaurantsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'), 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+    }), 
+    RestaurantsModule],
 })
 export class AppModule {}
